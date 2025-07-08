a46341846e0e1d1a5d27c97fdb2043a8ee680150
hardiksharma27 <hardiksharma13026q@gmail.com>
2025-07-08 15:41:13 +0530

    Update day7 Q3.py

--- day7 Q3.py
+++ day7 Q3.py
@@ -1,3 +1,10 @@
 fruits = ["apple","banana","mango","guava","pineapple"]
 print("Second Fruit : ", fruits[1])
 print("Last Fruit : ", fruits[-1])
+
+OUTPUT ---
+
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day7 Q3.py"
+
+Second Fruit :  banana
+Last Fruit :  pineapple
