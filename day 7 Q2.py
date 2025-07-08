fa3fb230dfa905352791573fa8d92cd3295d76e2
hardiksharma27 <hardiksharma13026q@gmail.com>
2025-07-08 15:40:32 +0530

    Update day 7 Q2.py

--- day 7 Q2.py
+++ day 7 Q2.py
@@ -1,3 +1,9 @@
 num = [23,3,33,65,3,83,98,3]
 count = num.count(3)
 print(count)
+
+OUTPUT ----
+
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 7 Q2.py"
+
+3
