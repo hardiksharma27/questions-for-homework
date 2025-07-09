15d274e5deb67bc35c71b378f85b530d7e012365
hardiksharma27 <hardiksharma13026q@gmail.com>
2025-07-09 17:25:02 +0530

    Update day 8 Question.py

--- day 8 Question.py
+++ day 8 Question.py
@@ -29,6 +29,30 @@ print("Length of tup2 " , len(tup2))
 print("Length of tup3 " , len(tup3))
 print("Length of tup4 " , len(tup4))
 
+
+OUTPUT -----
+
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 8 Question.py"
+ Original tup1  (27, 45.18, False, 'hardik')
+ Original tup2  (23, 3.14, True, 'sharma')
+ Updated tup1  (65, 76, True, 'Manik')
+ updated tup2  (32, 87.2, False, 'Aryan')
+ First element of tup1  65
+ Second element of tup1  76
+ Third element of tup1  True
+ First element of tup2  32
+ Second element of tup2  87.2
+ Third element of tup2  False
+Count of 12  2
+Count of 23  3
+Length of tup1  4
+Length of tup2  4
+Length of tup3  4
+Length of tup4  6
+ Concatenated tuple(tup+tup2) (65, 76, True, 'Manik', 32, 87.2, False, 'Aryan')
+Repeated tuple 19 times  (65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan', 65, 76, True, 'Manik', 32, 87.2, False, 'Aryan')
+PS C:\next yug classes> 
+
 #concatenation
 joined_tup = tup1 + tup2
 print(" Concatenated tuple(tup+tup2)", joined_tup)
