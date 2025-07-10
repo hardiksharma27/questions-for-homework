3743bf7a39e2e9c4dce40d3c41de62e63b3d243b
hardiksharma27 <hardiksharma13026q@gmail.com>
2025-07-10 12:03:14 +0530

    Update day 9 questions .py

--- day 9 questions .py
+++ day 9 questions .py
@@ -9,4 +9,16 @@ student_name = input("Enter the student's name: ")
 if student_name in student_marks:
     print( "Marks :" , student_marks[student_name])
 else:
-    print("Student not found.")
+    print("Student not found")
+
+OUTPUT --- 
+
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 9 questions .py"
+Enter the student's name: Alice
+Marks : 85
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 9 questions .py"
+Enter the student's name: Charlie  
+Marks : 78
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 9 questions .py"
+Enter the student's name: Bob
+Marks : 92
