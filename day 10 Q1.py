bf7e22c25215f89fc46bee40f5207c737e59b30d
hardiksharma27 <hardiksharma13026q@gmail.com>
2025-07-11 11:47:21 +0530

    Update day 10 Q1.py

--- day 10 Q1.py
+++ day 10 Q1.py
@@ -2,3 +2,18 @@ num = int(input("Enter a number: "))
 
 for i in range(1, 11):
     print(num * i)
+
+OUTPUT ---
+
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 10 Q1.py"
+Enter a number: 76
+76
+152
+228
+304
+380
+456
+532
+608
+684
+760
