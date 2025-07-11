c81baff0bb39dc8714d3a8520cafca61930ef1a3
hardiksharma27 <hardiksharma13026q@gmail.com>
2025-07-11 11:50:10 +0530

    Update day 10 Q2.py

--- day 10 Q2.py
+++ day 10 Q2.py
@@ -1,6 +1,13 @@
-lst = [1, 4, 6, 4, 9]
-t = 0
+num = [1, 4, 6, 4, 9]
+ttl = 0
 
-for i in lst:
-    t += i 
-    print("Sum:", t)
+for i in num:
+    ttl += i  
+
+print("Sum:", ttl)
+
+
+OUTPUT ----
+
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 10 Q2.py"
+Sum: 24
